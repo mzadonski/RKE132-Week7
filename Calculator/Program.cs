5270b18cee8afce5470d4caa0a11cfd101ca8e7b
MZadonski <maria.zadons@tktk.ee>
2023-05-07 22:33:48 +0300

    creat new app

---
+++ Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Enter the operation (+/-)");
+char userOperator = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a:");
+int firsNum = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Enter b:");
+int secondNum = Int32.Parse(Console.ReadLine());
+
+switch (userOperator)
+{
+    case '+':
+        Addition(firsNum, secondNum);
+        break;
+    case '-':
+        Subtraction(firsNum, secondNum);
+        break;
+    defult:
+        Console.WriteLine("invalid operator");
+        break;
+}
+
+
+static void Addition(int a, int b)
+{
+    Console.WriteLine($"{a} + {b} = {a + b}");
+}
+
+static void Subtraction(int a, int b)
+{
+    Console.WriteLine($"{a} - {b} = {a - b}");
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBuy();
+}
+
+
+
+static void PrintHello()  //function / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodBuy()
+{
+    Console.WriteLine("See you later, aligator,");
+}
